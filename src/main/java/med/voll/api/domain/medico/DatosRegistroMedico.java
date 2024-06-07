bece4c2c712211f42c1ca0f584dbda785c972043
freedox10 <alfre2636@yahoo.com.ar>
2024-06-06 22:31:28 -0300

    Trabaja manejo Errores API Rest 2024-05-06 Cs

--- src/main/java/med/voll/api/domain/medico/DatosRegistroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DatosRegistroMedico.java
@@ -1,24 +1,28 @@
-package med.voll.api.paciente;
+package med.voll.api.domain.medico;
+
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import med.voll.api.direccion.DatosDireccion;
+import med.voll.api.domain.direccion.DatosDireccion;
 
-public record DatosRegistroPaciente (
+public record DatosRegistroMedico(
         @NotBlank
         String nombre,
-        @NotBlank @Email
+        @NotBlank
+        @Email
         String email,
         @NotBlank
         String telefono,
-        @NotBlank @Pattern(regexp = "\\d{3}\\.?\\d{3}\\.?\\d{3}\\-?\\d{2}")
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
         String documento,
+        @NotNull
+        Especialidad especialidad,
         @NotNull
         @Valid
         DatosDireccion direccion
 ) {
-
 }
