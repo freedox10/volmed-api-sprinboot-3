63523cc9408e90f86fb353f50a80d41da716bd52
freedox10 <alfre2636@yahoo.com.ar>
2024-06-14 01:31:49 -0300

    Trabaja Test 2024-06-13 Cs

--- src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
+++ src/test/java/med/voll/api/domain/medico/MedicoRepositoryTest.java
@@ -1,10 +1,24 @@
 package med.voll.api.domain.medico;
 
+import med.voll.api.domain.consulta.Consulta;
+import med.voll.api.domain.direccion.DatosDireccion;
+import med.voll.api.domain.paciente.DatosRegistroPaciente;
+import med.voll.api.domain.paciente.Paciente;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
@@ -12,7 +26,76 @@ import static org.junit.jupiter.api.Assertions.*;
 @ActiveProfiles("test")
 class MedicoRepositoryTest {
 
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private TestEntityManager em;
+
     @Test
-    void seleccionarMedicoConEspecialidadEnFecha() {
+    @DisplayName("deberia retornar nulo cuando el medico se encuentre en consulta con otro paciente en ese horario")
+    void seleccionarMedicoConEspecialidadEnFechaEscenario1() {
+
+        //given
+        var proximoLunes10H = LocalDate.now()
+                .with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+                .atTime(10, 0);
+
+        var medico= registrarMedico("Jose Moro", "jm@gmail.com", "456465", Especialidad.GINECOLOGIA);
+        var paciente= registrarPaciente("Antonio Gil", "ag@gmail.com", "456872");
+        registarConsulta(medico, paciente, proximoLunes10H);
+
+        //when
+        var medicoLibre =medicoRepository.seleccionarMedicoConEspecialidadEnFecha(Especialidad.GINECOLOGIA, proximoLunes10H);
+
+        //then
+        assertThat(medicoLibre).isNull();
+    }
+
+    private void registarConsulta(Medico medico, Paciente paciente, LocalDateTime fecha){
+        em.persist(new Consulta(null, medico, paciente, fecha, null));
+    }
+
+    private Medico registrarMedico(String nombre, String email, String documento, Especialidad especialidad){
+        var medico = new Medico(datosMedico(nombre, email, documento, especialidad));
+        em.persist(medico);
+        return medico;
+    }
+
+    private Paciente registrarPaciente(String nombre, String email, String documento){
+        var paciente = new Paciente(datosPaciente(nombre, email, documento));
+        em.persist(paciente);
+        return paciente;
+    }
+
+    private DatosRegistroMedico datosMedico(String nombre, String email, String documento, Especialidad especialidad){
+        return new DatosRegistroMedico(
+                nombre,
+                email,
+                "6262465497",
+                documento,
+                especialidad,
+                datosDireccion()
+        );
+    }
+
+    private DatosRegistroPaciente datosPaciente(String nombre, String email, String documento){
+        return new DatosRegistroPaciente(
+                nombre,
+                email,
+                "5572279517",
+                documento,
+                datosDireccion()
+        );
+    }
+
+    private DatosDireccion datosDireccion(){
+        return new DatosDireccion(
+                "Luca",
+                "1649",
+                "Azul",
+                "Acapulco",
+                "16"
+        );
     }
 }
